5b500c93623aff949184f4e53ed500ed84537072
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-22 18:24:48 +0100

    Added Vehicle DTO

---
+++ CarRentalAPI/Models/DTO/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using CarRentalAPI.Models.Domain;
+
+namespace CarRentalAPI.Models.DTO
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public Color Color { get; set; } //By default it will use System.Drawing
+        public DateOnly YearOfProduction { get; set; }
+
+        //public int? UserId { get; set; }
+    }
+}

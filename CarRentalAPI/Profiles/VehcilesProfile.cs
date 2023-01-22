13db03ef5dc779f17ed26a88b7b233617e55c115
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-22 18:24:57 +0100

    Added VehiclesProfile

---
+++ CarRentalAPI/Profiles/VehcilesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CarRentalAPI.Profiles
+{
+    public class VehcilesProfile : Profile
+    {
+        public VehcilesProfile()
+        {
+            CreateMap<Models.Domain.Vehicle, Models.DTO.Vehicle>()
+                .ReverseMap();
+        }
+    }
+}

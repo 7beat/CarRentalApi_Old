cf9fe761297bc7ae5b18e18f2597803d5f5d205f
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-22 23:06:57 +0100

    AddVehicleRequest

---
+++ CarRentalAPI/Models/DTO/AddVehicleRequest.cs
@@ -0,0 +1,12 @@
+﻿using CarRentalAPI.Models.Domain;
+
+namespace CarRentalAPI.Models.DTO
+{
+    public class AddVehicleRequest
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Color { get; set; }
+        public DateOnly YearOfProduction { get; set; }
+    }
+}

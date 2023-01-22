999bbc6353768aea49fa8bc0e9dc4910ea112b99
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-22 18:09:04 +0100

    Renamed to VehiclesController

---
+++ CarRentalAPI/Controllers/VehiclesController.cs
@@ -0,0 +1,29 @@
+﻿using CarRentalAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IVehicleRepository vehicleRepository;
+        
+        public VehiclesController(Repositories.IVehicleRepository vehicleRepository)
+        {
+            this.vehicleRepository = vehicleRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllVehicles()
+        {
+            var vehiclesDomain = await vehicleRepository.GetAllAsync();
+
+            //Map to DTO
+            //var vehiclesDTO
+
+            return Ok(vehiclesDomain);
+        }
+    }
+}

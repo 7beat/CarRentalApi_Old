1c399dc1f97fa00093da0ef6be836e72574d23cf
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-22 18:08:11 +0100

    Added GetAllAsync

---
+++ CarRentalAPI/Repositories/IVehicleRepository.cs
@@ -0,0 +1,10 @@
+﻿using CarRentalAPI.Models.Domain;
+
+namespace CarRentalAPI.Repositories
+{
+    public interface IVehicleRepository
+    {
+        Task<IEnumerable<Vehicle>> GetAllAsync();
+        Task<Vehicle> GetByIdAsync(int id);
+    }
+}

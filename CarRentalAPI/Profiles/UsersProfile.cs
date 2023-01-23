fe8cf6cf1dad133503b929e66fe03cefd3f7316f
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-24 00:01:15 +0100

    Added proper user profile

---
+++ CarRentalAPI/Profiles/UsersProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace CarRentalAPI.Profiles
+{
+    public class UsersProfile : Profile
+    {
+        public UsersProfile()
+        {
+            CreateMap<Models.Domain.User, Models.DTO.User>()
+                .ReverseMap();
+        }
+    }
+}

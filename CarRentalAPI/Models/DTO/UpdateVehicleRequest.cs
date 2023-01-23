cb55a3e8d33e6f5826c9708eb85c728bd2d61bce
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-23 13:43:19 +0100

    Added UpdateVehicleRequest

---
+++ CarRentalAPI/Models/DTO/UpdateVehicleRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CarRentalAPI.Models.DTO
+{
+    public class UpdateVehicleRequest
+    {
+        public string Model { get; set; } //WIP
+        public int Color { get; set; }
+    }
+}

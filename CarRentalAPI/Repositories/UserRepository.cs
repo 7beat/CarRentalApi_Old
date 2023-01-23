da3e7cb01b6090f65ec0579253b31c04627e397b
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-23 22:51:02 +0100

    Implemented GetAllAsync

---
+++ CarRentalAPI/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using CarRentalAPI.Data;
+using CarRentalAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalAPI.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _appDbContext.Users
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.Color)
+                .ToListAsync();
+        }
+    }
+}

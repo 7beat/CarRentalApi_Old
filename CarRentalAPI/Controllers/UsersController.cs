7fde229c14075bd299584be9f9f0cdefbb9521ea
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-23 22:49:53 +0100

    Added GetAllAsync

---
+++ CarRentalAPI/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CarRentalAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository vehicleRepository;
+        private readonly IMapper mapper;
+
+        public UsersController(Repositories.IUserRepository vehicleRepository, IMapper mapper)
+        {
+            this.vehicleRepository = vehicleRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var usersDomain = await vehicleRepository.GetAllAsync();
+
+            //Convert to DTO
+            //var usersDTO = mapper.Map
+
+            return Ok(usersDomain);
+        }
+    }
+}

d72c66ffd4a7089e4049daa7590eced65d29f05c
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-24 17:07:22 +0100

    Implemented IsEmailUnique and IsUsernameUnique

--- CarRentalAPI/Repositories/UserRepository.cs
+++ CarRentalAPI/Repositories/UserRepository.cs
@@ -13,6 +13,14 @@ namespace CarRentalAPI.Repositories
             _appDbContext = appDbContext;
         }
 
+        public async Task<User> AddAsync(User user)
+        {
+            await _appDbContext.Users.AddAsync(user);
+            await _appDbContext.SaveChangesAsync();
+            //var test = _appDbContext;
+            return await GetByIdAsync(user.Id);
+        }
+
         public async Task<IEnumerable<User>> GetAllAsync()
         {
             return await _appDbContext.Users
@@ -27,6 +35,17 @@ namespace CarRentalAPI.Repositories
                 .Include(x => x.Vehicles)
                 .ThenInclude(x => x.Color)
                 .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        //Testing
+        public async Task<bool> IsEmailUnique(string email)
+        {
+            return !await _appDbContext.Users.AnyAsync(x => x.Email == email);
+        }
+
+        public async Task<bool> IsUsernameUnique(string username)
+        {
+            return !await _appDbContext.Users.AnyAsync(x => x.Username == username);
         }
     }
 }

c71ff2f8fb48f5c0d753d60f38afda02cbe570ff
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-24 18:36:49 +0100

    Added UpdateUserAsync

--- CarRentalAPI/Controllers/UsersController.cs
+++ CarRentalAPI/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CarRentalAPI.Models.Domain;
 using CarRentalAPI.Models.DTO;
 using CarRentalAPI.Repositories;
 using Microsoft.AspNetCore.Http;
@@ -63,6 +64,33 @@ namespace CarRentalAPI.Controllers
             return Ok(userDTO);
         }
 
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> UpdateUserAsync(int id,
+            Models.DTO.UpdateUserRequest updateUserRequest)
+        {
+            if (!await ValidateUpdateUserRequestAsync(updateUserRequest))
+                return BadRequest(ModelState);
+
+            var userDomain = new Models.Domain.User
+            {
+                Username = updateUserRequest.Username,
+                FirstName = updateUserRequest.FirstName,
+                LastName = updateUserRequest.LastName,
+                BirthDay = updateUserRequest.BirthDay
+            };
+
+            userDomain = await userRepository.UpdateAsync(id, userDomain);
+
+            if (userDomain is null)
+                return NotFound();
+
+            var userDTO = mapper.Map<Models.DTO.User>(userDomain);
+
+            return Ok(userDTO);
+        }
+
+
         private async Task<bool> ValidateAddUserRequestAsync(Models.DTO.AddUserRequest user)
         {
             if (!await userRepository.IsEmailUnique(user.Email))
@@ -77,6 +105,16 @@ namespace CarRentalAPI.Controllers
                 return false;
             }
 
+            return true;
+        }
+
+        private async Task<bool> ValidateUpdateUserRequestAsync(Models.DTO.UpdateUserRequest updateUser)
+        {
+            if (!await userRepository.IsUsernameUnique(updateUser.Username))
+            {
+                ModelState.AddModelError(nameof(updateUser), $"{nameof(updateUser.Username)} is already taken!");
+                return false;
+            }
             return true;
         }
     }

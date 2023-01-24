db75baeb28e4765a9f95d4bdec0fe480593f2963
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-24 19:01:29 +0100

    Added DeleteAsync

--- CarRentalAPI/Repositories/IUserRepository.cs
+++ CarRentalAPI/Repositories/IUserRepository.cs
@@ -9,6 +9,7 @@ namespace CarRentalAPI.Repositories
         Task<User> GetByIdAsync(int id);
         Task<User> AddAsync(User user);
         Task<User> UpdateAsync(int id, User user);
+        Task<User> DeleteAsync(int id);
         Task<bool> IsEmailUnique(string email);
         Task<bool> IsUsernameUnique(string username);
     }

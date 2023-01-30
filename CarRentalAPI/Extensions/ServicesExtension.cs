0d0635eb905a7adb668351bc44bb6611cad82275
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-30 23:13:49 +0100

    Added new configurations

---
+++ CarRentalAPI/Extensions/ServicesExtension.cs
@@ -0,0 +1,139 @@
+﻿using AutoMapper;
+using CarRentalAPI.Data;
+using CarRentalAPI.Models.Identity;
+using CarRentalAPI.Profiles;
+using CarRentalAPI.Repositories;
+using CarRentalAPI.Repositories.Interfaces;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace CarRentalAPI.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+        }
+
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentity<AppUser, IdentityRole<int>>(o =>
+            {
+                o.Password.RequireDigit = false;
+                o.Password.RequireUppercase = false;
+
+                o.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+        }
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtConfig = configuration.GetSection("Jwt");
+            var secretKey = jwtConfig["Key"];
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtConfig["Issuer"], //ValidIssuer
+                    ValidAudience = jwtConfig["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Car Rental API",
+                    Version = "v1",
+                    Description = "Simple car renting API Services",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "7beat",
+                        Email = "t_krzem123@wp.pl",
+                        Url = new("https://github.com/7beat")
+                    },
+                });
+                c.EnableAnnotations();
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+        }
+
+        public static void ConfigureMapping(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
+            var mapperConfig = new MapperConfiguration(map =>
+            {
+                map.AddProfile<UsersProfile>();
+                map.AddProfile<VehiclesProfile>();
+                map.AddProfile<UserMappingProfile>();
+            });
+            services.AddSingleton(mapperConfig.CreateMapper());
+        }
+
+        public static void ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IVehicleRepository, VehicleRepository>();
+            services.AddScoped<IUserAuthenticationRepository, UserAuthenticationRepository>();
+        }
+
+        public static void ConfigureFluentValidation(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation()
+                .AddFluentValidationClientsideAdapters()
+                .AddValidatorsFromAssemblyContaining<Program>();
+        }
+    }
+}

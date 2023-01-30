61e136e9de1dc0f91347ba025e77bc5da682ea41
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-30 19:30:43 +0100

    Added UserConfiguration

--- CarRentalAPI/Extensions/ServiceExtension.cs
+++ CarRentalAPI/Extensions/ServiceExtension.cs
@@ -1,6 +1,23 @@
-﻿namespace CarRentalAPI.Extensions
+﻿using CarRentalAPI.Data;
+using CarRentalAPI.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace CarRentalAPI.Extensions
 {
     public static class ServiceExtension
     {
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentity<AppUser, IdentityRole<int>>(o =>
+            {
+                o.Password.RequireDigit = false;
+                o.Password.RequireUppercase = false;
+
+                o.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+        }
     }
 }

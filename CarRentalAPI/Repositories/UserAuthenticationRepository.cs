2f8b0e5b9b63970aebe42169d5f4fd19ada036ae
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-30 22:38:11 +0100

    Impemented Token Generation

--- CarRentalAPI/Repositories/UserAuthenticationRepository.cs
+++ CarRentalAPI/Repositories/UserAuthenticationRepository.cs
@@ -4,6 +4,11 @@ using CarRentalAPI.Models.DTO;
 using CarRentalAPI.Models.Identity;
 using CarRentalAPI.Repositories.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace CarRentalAPI.Repositories
 {
@@ -11,12 +16,15 @@ namespace CarRentalAPI.Repositories
     {
 
         private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration _configuration;
         private readonly IMapper _mapper;
+        private AppUser? _user;
 
-        public UserAuthenticationRepository(UserManager<AppUser> userManager, IMapper mapper)
+        public UserAuthenticationRepository(UserManager<AppUser> userManager, IMapper mapper, IConfiguration configuration)
         {
             _userManager = userManager;
             _mapper = mapper;
+            _configuration = configuration;
         }
 
         public async Task<IdentityResult> RegisterUserAsync(UserRegistrationDto userRegistration)
@@ -28,15 +36,59 @@ namespace CarRentalAPI.Repositories
 
         public async Task<bool> ValidateUserAsync(UserLoginDto loginDto)
         {
-            var user = await _userManager.FindByNameAsync(loginDto.Username);
-
-            var result = user != null && await _userManager.CheckPasswordAsync(user, loginDto.Password);
+            //var user = await _userManager.FindByNameAsync(loginDto.Username);
+            _user = await _userManager.FindByNameAsync(loginDto.Username);
+            var result = _user != null && await _userManager.CheckPasswordAsync(_user, loginDto.Password);
             return result;
         }
 
-        public Task<string> CreateTokenAsync()
+        //public Task<string> CreateTokenAsync()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public async Task<string> CreateTokenAsync()
         {
-            throw new NotImplementedException();
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            //return "lol";        
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var jwtConfig = _configuration.GetSection("Jwt"); //Rename to Jwt
+            var key = Encoding.UTF8.GetBytes(jwtConfig["Key"]);
+            var secret = new SymmetricSecurityKey(key);
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, _user.UserName)
+            };
+            var roles = await _userManager.GetRolesAsync(_user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var tokenOptions = new JwtSecurityToken
+            (
+            issuer: jwtSettings["Issuer"],
+            audience: jwtSettings["Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(15),
+            signingCredentials: signingCredentials
+            );
+            return tokenOptions;
         }
 
         //public Task<IdentityResult> RegisterUserAsync(AppUser userRegistration)

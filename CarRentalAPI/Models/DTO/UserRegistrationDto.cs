1d63adbd9746cf77669317ece74477ac2fce92f4
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-30 20:13:34 +0100

    Added UserRegistrationDto

---
+++ CarRentalAPI/Models/DTO/UserRegistrationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalAPI.Models.DTO
+{
+    public class UserRegistrationDto
+    {
+        public string? FirstName { get; init; }
+        public string? LastName { get; init; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string? UserName { get; init; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; init; }
+    }
+}

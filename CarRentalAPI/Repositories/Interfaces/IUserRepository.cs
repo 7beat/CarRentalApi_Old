a533efd9edddfaf1ce1c979f0859ca6beb63988c
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-30 23:44:55 +0100

    Moved to sub folder

---
+++ CarRentalAPI/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using CarRentalAPI.Data;
+using CarRentalAPI.Models.Domain;
+
+namespace CarRentalAPI.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int id);
+        Task<User> AddAsync(User user);
+        Task<User> UpdateAsync(int id, User user);
+        Task<User> DeleteAsync(int id);
+        Task<bool> IsEmailUnique(string email);
+        Task<bool> IsUsernameUnique(string username);
+    }
+}
---
+++ CarRentalAPI/Repositories/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,13 @@
+﻿using CarRentalAPI.Models.Domain;
+
+namespace CarRentalAPI.Repositories.Interfaces
+{
+    public interface IVehicleRepository
+    {
+        Task<IEnumerable<Vehicle>> GetAllAsync();
+        Task<Vehicle> GetByIdAsync(int id);
+        Task<Vehicle> AddAsync(Vehicle vehicle);
+        Task<Vehicle> UpdateAsync(int id, Vehicle vehicle);
+        Task<Vehicle> DeteleAsync(int id);
+    }
+}

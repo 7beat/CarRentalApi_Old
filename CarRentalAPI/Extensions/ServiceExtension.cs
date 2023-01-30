b8197c040e097d7ed7ae4d2d9eca364a06ce7005
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-30 20:13:24 +0100

    Added services

--- CarRentalAPI/Extensions/ServiceExtension.cs
+++ CarRentalAPI/Extensions/ServiceExtension.cs
@@ -1,6 +1,12 @@
-﻿using CarRentalAPI.Data;
+﻿using AutoMapper;
+using CarRentalAPI.Data;
 using CarRentalAPI.Models.Identity;
+using CarRentalAPI.Profiles;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace CarRentalAPI.Extensions
 {
@@ -19,5 +25,42 @@ namespace CarRentalAPI.Extensions
             .AddEntityFrameworkStores<AppDbContext>()
             .AddDefaultTokenProviders();
         }
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtConfig = configuration.GetSection("Jwt"); //JwtConfig
+            var secretKey = jwtConfig["Key"]; //Secret
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtConfig["Issuer"], //ValidIssuer
+                    ValidAudience = jwtConfig["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+        }
+
+        //public static void ConfigureMapping(this IServiceCollection services)
+        //{
+        //    services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
+        //    var mapperConfig = new MapperConfiguration(map =>
+        //    {
+        //        map.AddProfile<UsersProfile>();
+        //        map.AddProfile<VehiclesProfile>();
+        //        //map.AddProfile<UserMappingProfile>();
+        //    });
+        //    services.AddSingleton(mapperConfig.CreateMapper());
+        //}
+
     }
 }

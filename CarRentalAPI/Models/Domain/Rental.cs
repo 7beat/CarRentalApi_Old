5073508e190fd4a3b7aa4deeb5c4bd1d679fe02f
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-31 19:03:26 +0100

    Added proper user

--- CarRentalAPI/Models/Domain/Rental.cs
+++ CarRentalAPI/Models/Domain/Rental.cs
@@ -1,9 +1,11 @@
-﻿namespace CarRentalAPI.Models.Domain
+﻿using CarRentalAPI.Models.Identity;
+
+namespace CarRentalAPI.Models.Domain
 {
     public class Rental
     {
         public int Id { get; set; }
-        public User User { get; set; }
+        public AppUser User { get; set; }
         public Vehicle Vehicle { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }

469b1b83a30fa1394e347577f0bd55a298811a12
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-31 18:48:28 +0100

    Added UserUpdateDto

---
+++ CarRentalAPI/Models/Identity/UserUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalAPI.Models.Identity
+{
+    public class UserUpdateDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}

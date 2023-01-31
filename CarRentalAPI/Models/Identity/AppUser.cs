a8ab4391e83dc9a54fe3b4592122ae3b1824d819
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-31 12:58:17 +0100

    Added related vehicles

--- CarRentalAPI/Models/Identity/AppUser.cs
+++ CarRentalAPI/Models/Identity/AppUser.cs
@@ -5,9 +5,9 @@ namespace CarRentalAPI.Models.Identity
 {
     public class AppUser : IdentityUser<int>
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        //public DateOnly? Birthday { get; set; }
-        //public ICollection<Vehicle> Vehicles { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly Birthday { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; }
     }
 }

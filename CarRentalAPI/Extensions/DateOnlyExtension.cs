7705eddd65a1abc6cd253fce035cc2f5628eb8f8
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-01 22:43:22 +0100

    Added comment

--- CarRentalAPI/Extensions/DateOnlyExtension.cs
+++ CarRentalAPI/Extensions/DateOnlyExtension.cs
@@ -1,8 +1,8 @@
 ﻿namespace CarRentalAPI.Extensions
 {
-    public static class DateOnlyExtensions //Add to Extensions folder in CarRentalAPI!
+    public static class DateOnlyExtensions
     {
-        public static bool IsInRange(this DateOnly now, DateOnly start, DateOnly end)
+        public static bool IsInRange(this DateOnly now, DateOnly start, DateOnly end) // Is this date beetween 2 dates?
         {
             return now >= start && now <= end;
         }

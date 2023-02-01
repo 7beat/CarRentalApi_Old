ff86d0832ff1c64e75c8443b0ce20fc7e8e4160d
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-01 22:40:34 +0100

    Created IsInRange DateOnly Extension

--- CarRentalAPI/Extensions/DateOnlyExtension.cs
+++ CarRentalAPI/Extensions/DateOnlyExtension.cs
@@ -1,6 +1,10 @@
 ﻿namespace CarRentalAPI.Extensions
 {
-    public static class DateOnlyExtension
+    public static class DateOnlyExtensions //Add to Extensions folder in CarRentalAPI!
     {
+        public static bool IsInRange(this DateOnly now, DateOnly start, DateOnly end)
+        {
+            return now >= start && now <= end;
+        }
     }
 }

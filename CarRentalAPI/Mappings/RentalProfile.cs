f95ab2254c8eca4b9e7c19af6dbd75efc09b2596
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-01 20:02:14 +0100

    Created Rental Profile

---
+++ CarRentalAPI/Mappings/RentalProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace CarRentalAPI.Mappings
+{
+    public class RentalProfile : Profile
+    {
+        public RentalProfile()
+        {
+            CreateMap<Models.Domain.Rental, Models.DTO.Rental>()
+                .ReverseMap();
+        }
+    }
+}

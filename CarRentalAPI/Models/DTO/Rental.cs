03725e87e82181c4468763243df60381f2eb517c
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-01 20:02:28 +0100

    Added RentalDto

---
+++ CarRentalAPI/Models/DTO/Rental.cs
@@ -0,0 +1,13 @@
+﻿using CarRentalAPI.Models.Identity;
+
+namespace CarRentalAPI.Models.DTO
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        //public AppUser User { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+    }
+}

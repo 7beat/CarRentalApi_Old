833d77c2c29a28fde08a9a567eb8bd030af25f28
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-02 13:32:03 +0100

    Added documentation

--- CarRentalAPI/Extensions/DateOnlyExtension.cs
+++ CarRentalAPI/Extensions/DateOnlyExtension.cs
@@ -2,7 +2,14 @@
 {
     public static class DateOnlyExtensions
     {
-        public static bool IsInRange(this DateOnly now, DateOnly start, DateOnly end) // Is this date beetween 2 dates?
+        /// <summary>
+        /// Checks whether given date is between two other dates
+        /// </summary>
+        /// <param name="now"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns>true if given date happens to be in range of two other dates</returns>
+        public static bool IsInRange(this DateOnly now, DateOnly start, DateOnly end)
         {
             return now >= start && now <= end;
         }

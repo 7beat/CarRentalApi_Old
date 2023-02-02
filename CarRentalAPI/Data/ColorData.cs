7f7fbd272055af7c691cf92916904223a2889197
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-02 23:30:07 +0100

    Seed data

---
+++ CarRentalAPI/Data/ColorData.cs
@@ -0,0 +1,25 @@
+﻿using CarRentalAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRentalAPI.Data
+{
+    public class ColorData : IEntityTypeConfiguration<Models.Domain.Color>
+    {
+        public void Configure(EntityTypeBuilder<Color> builder)
+        {
+            builder.HasData(
+                new Color
+                {
+                    Id = 1,
+                    Name = "Black",
+                },
+                new Color
+                {
+                    Id = 2,
+                    Name= "White",
+                }
+                );
+        }
+    }
+}
---
+++ CarRentalAPI/Data/VehicleData.cs
@@ -0,0 +1,39 @@
+﻿using CarRentalAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRentalAPI.Data
+{
+    public class VehicleData : IEntityTypeConfiguration<Vehicle>
+    {
+        public void Configure(EntityTypeBuilder<Vehicle> builder)
+        {
+            builder.HasData(
+                new Vehicle
+                {
+                    Id = 1,
+                    Brand = "Ford",
+                    Model = "Mondeo",
+                    YearOfProduction = new(2017, 2, 15),
+                    ColorId = 1,
+                },
+                new Vehicle
+                {
+                    Id = 2,
+                    Brand = "Mercedes",
+                    Model = "GLC",
+                    YearOfProduction = new(2019, 6, 8),
+                    ColorId = 1,
+                },
+                new Vehicle
+                {
+                    Id = 3,
+                    Brand = "Honda",
+                    Model = "Civic",
+                    YearOfProduction = new(2008, 8, 21),
+                    ColorId = 2,
+                }
+                );
+        }
+    }
+}

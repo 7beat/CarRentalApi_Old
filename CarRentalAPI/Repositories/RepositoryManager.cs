9319225b95479504e71456cf681c86a4347900e4
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-05 19:12:31 +0100

    Implemented RepositoryManager

---
+++ CarRentalAPI/Repositories/RepositoryManager.cs
@@ -0,0 +1,31 @@
+﻿using CarRentalAPI.Data;
+using CarRentalAPI.Repositories.Interfaces;
+
+namespace CarRentalAPI.Repositories
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly AppDbContext dbContext;
+        private IVehicle2Repository _vehicle2Repository;
+
+        public RepositoryManager(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IVehicle2Repository Vehicle
+        {
+            get
+            {
+                if (_vehicle2Repository is null)
+                    _vehicle2Repository = new Vehicle2Repository(dbContext);
+                return _vehicle2Repository;
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}

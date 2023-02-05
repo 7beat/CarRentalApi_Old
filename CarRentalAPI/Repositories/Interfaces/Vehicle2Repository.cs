2abe968a0bba60b43e40672892a1da65ae2c7ef3
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-05 18:07:54 +0100

    Added test repos

---
+++ CarRentalAPI/Repositories/Interfaces/Vehicle2Repository.cs
@@ -0,0 +1,30 @@
+﻿using CarRentalAPI.Data;
+using CarRentalAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalAPI.Repositories.Interfaces
+{
+    public class Vehicle2Repository : RepositoryBase<Vehicle>, IVehicle2Repository
+    {
+        public Vehicle2Repository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+
+        //public async Task<Vehicle> GetVehicle(int id)
+        //    => await FindByConditionAsync(e => e.TeacherId.Equals(teacherId) && e.Id.Equals(studentId), trackChanges).Result.SingleOrDefaultAsync();
+
+        public async Task<Vehicle> GetVehicle(int id, bool trackChanges)
+            => await FindByConditionAsync(e => e.Id.Equals(id), trackChanges).Result.Include(x => x.Color).SingleOrDefaultAsync();
+
+        //await FindByConditionAsync(c => c.Id.Equals(teacherId), trackChanges).Result.Include
+
+        public async Task CreateVehicle(Vehicle vehicle) => await CreateAsync(vehicle);
+
+        public async Task<IEnumerable<Vehicle>> GetAllVehicles(bool trackChanges)
+        {
+            //var vehicles = await FindAllAsync(trackChanges).Result.Include(x => x.Color);
+            return await FindAllAsync(trackChanges).Result.Include(x => x.Color).ToListAsync();
+        }
+    }
+}

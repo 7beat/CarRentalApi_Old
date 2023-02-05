78a82f87c747912a1fc990d1801d382dad38daf3
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-05 19:12:13 +0100

    Added IRepositoryManager

---
+++ CarRentalAPI/Repositories/Interfaces/IRepositoryManager.cs
@@ -0,0 +1,9 @@
+﻿namespace CarRentalAPI.Repositories.Interfaces
+{
+    public interface IRepositoryManager
+    {
+        IVehicle2Repository Vehicle { get; }
+        //IUserAuthenticationRepository UserAuthentication { get; }
+        Task SaveAsync();
+    }
+}

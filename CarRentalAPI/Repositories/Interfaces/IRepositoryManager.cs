0cd0317049f9f9dc929b2d72ad5700ba914d989e
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-07 12:18:22 +0100

    Added IUserAuthenticationRepository

--- CarRentalAPI/Repositories/Interfaces/IRepositoryManager.cs
+++ CarRentalAPI/Repositories/Interfaces/IRepositoryManager.cs
@@ -5,6 +5,7 @@
         IVehicle2Repository Vehicle2 { get; }
         //IUserAuthenticationRepository UserAuthentication { get; }
         IVehicleRepository Vehicles { get; }
+        IUserAuthenticationRepository UserAuthentication { get; }
         Task SaveAsync();
     }
 }

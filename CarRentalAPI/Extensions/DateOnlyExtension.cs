98ac7949e52e651e2f89f92e9e8089f2d7357de7
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-07 02:56:51 +0100

    Docs tweak

--- CarRentalAPI/Extensions/DateOnlyExtension.cs
+++ CarRentalAPI/Extensions/DateOnlyExtension.cs
@@ -8,7 +8,7 @@
         /// <param name="now"></param>
         /// <param name="start"></param>
         /// <param name="end"></param>
-        /// <returns>True if given date happens to be in range of two other dates, otherwise false.</returns>
+        /// <returns><see langword="true"/> if given date happens to be in range of two other dates, otherwise <see langword="false"/>.</returns>
         public static bool IsInRange(this DateOnly now, DateOnly start, DateOnly end)
         {
             return now >= start && now <= end;

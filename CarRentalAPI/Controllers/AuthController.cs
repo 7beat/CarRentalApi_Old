7fab5891e3e9757a5c2319c3b7311acfab38bd21
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-09 23:29:23 +0100

    Added email sending

--- CarRentalAPI/Controllers/AuthController.cs
+++ CarRentalAPI/Controllers/AuthController.cs
@@ -1,10 +1,10 @@
-﻿using CarRentalAPI.Models.Domain;
-using CarRentalAPI.Models.DTO;
+﻿using CarRentalAPI.Models.DTO;
 using CarRentalAPI.Models.Identity;
 using CarRentalAPI.Repositories.Interfaces;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
+using System.Text.Encodings.Web;
 
 namespace CarRentalAPI.Controllers
 {
@@ -13,10 +13,12 @@ namespace CarRentalAPI.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IRepositoryManager repository;
+        private readonly IEmailSender emailSender;
 
-        public AuthController(IRepositoryManager repository)
+        public AuthController(IRepositoryManager repository, IEmailSender emailSender)
         {
             this.repository = repository;
+            this.emailSender = emailSender;
         }
 
         [HttpPost]
@@ -25,6 +27,23 @@ namespace CarRentalAPI.Controllers
         public async Task<IActionResult> RegisterUser([FromBody] UserRegistrationDto registerRequest)
         {
             var userResult = await repository.UserAuthentication.RegisterUserAsync(registerRequest);
+
+#if true // Email confirmation
+
+            if (userResult.Succeeded)
+            {
+                var tokenData = await repository.UserAuthentication.CreateEmailCredentials();
+
+                var callbackUrl = Url.Action("ConfirmEmail", "Auth",
+                    new { userId = tokenData.userId, code = tokenData.token }, Request.Scheme);
+
+                await emailSender.SendEmailAsync(registerRequest.Email, "Confirm your email",
+                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                return StatusCode(StatusCodes.Status201Created, "Confirmation email was sent!");
+            }
+
+#endif
             return !userResult.Succeeded ? new BadRequestObjectResult(userResult) : StatusCode(StatusCodes.Status201Created);
         }
 
@@ -35,7 +54,7 @@ namespace CarRentalAPI.Controllers
 
         [HttpPost]
         [Route("login")]
-        [SwaggerOperation(Summary = "Login to Account", 
+        [SwaggerOperation(Summary = "Login to Account",
             Description = "Provide correct Username and Password, Lockout is enabled 5 attempts will block account for 5 minutes!")]
         public async Task<IActionResult> Authenticate([FromForm] UserLoginDto loginRequest)
         {
@@ -46,6 +65,26 @@ namespace CarRentalAPI.Controllers
             else
             {
                 return Ok(new { Token = await repository.UserAuthentication.CreateTokenAsync() });
+            }
+        }
+
+        [HttpGet]
+        [Route("confirm-email")]
+        public async Task<IActionResult> ConfirmEmail(int userId, string code)
+        {
+            var user = await repository.Users.GetByIdAsync(userId);
+            if (user is null)
+                return BadRequest("Invalid user");
+            
+            var result = await repository.UserAuthentication.ConfirmUserEmail(user, code);
+
+            if (result.Succeeded)
+            {
+                return Ok("Email confirmed");
+            }
+            else
+            {
+                return BadRequest("Invalid confirmation code");
             }
         }
     }

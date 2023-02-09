62c7a5cc6a8be1bc89b672606f0a3eaddfdfbc6a
Tomasz Krzemiński <7beat123@wp.pl>
2023-02-09 23:31:54 +0100

    Revert "Revert "Implemented IEmailSender""
    This reverts commit 63b465d97d4e0d7429d82fdabcfe720d059b7b94.

---
+++ CarRentalAPI/Services/EmailSender.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+
+namespace CarRentalAPI.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public EmailSender()
+        {
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            string fromMail = "7beat123@gmail.com";
+            string fromPassword = "gogyydxgwwipxmzj";
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = subject;
+            message.To.Add(new MailAddress(email));
+            message.Body = "<html><body> " + htmlMessage + " </body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+            smtpClient.Send(message);
+            //await Task.Run(() => smtpClient.Send(message));
+        }
+    }
+}
